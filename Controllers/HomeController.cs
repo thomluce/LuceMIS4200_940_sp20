5e3ba214b276e217aa74cd6601146d763e7a4e23
Luce, Thom <luce@ohio.edu>
2020-01-24 10:42:37 -0500

    after second class

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace LuceMIS4200_940.Controllers
 {
@@ -15,14 +11,14 @@ namespace LuceMIS4200_940.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "My MIS4200 Test Description.";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Info about ME.";
 
             return View();
         }
