3dbe5edc263ea106d36e65989d612b4775d62a5f
Luce, Thom <luce@ohio.edu>
2020-01-27 10:43:58 -0500

    start modeling

---
+++ Models/customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LuceMIS4200_940.Models
+{
+    public class customer
+    {
+        public int customerID { get; set; }
+        public string customerLastName { get; set; }
+        public string customerFirstName { get; set; }
+        public string email { get; set; }
+        public string  phone { get; set; }
+        public DateTime customerSince { get; set; }
+
+    }
+}
