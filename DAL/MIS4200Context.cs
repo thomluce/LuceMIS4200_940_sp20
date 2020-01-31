8dbb05ea859069579dac0debfb7ad80b6f4b2978
Luce, Thom <luce@ohio.edu>
2020-01-31 10:39:46 -0500

    first edits

---
+++ DAL/MIS4200Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using LuceMIS4200_940.Models;
+
+namespace LuceMIS4200_940.DAL
+{
+    public class MIS4200Context : DbContext
+    {
+        public MIS4200Context() : base("name=DefaultConnection")
+        {
+
+        }
+        public DbSet<customer> Customers { get; set; }
+        public DbSet<Orders> Orders { get; set; }
+    }
+}
---
+++ Models/Orders.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LuceMIS4200_940.Models
+{
+    public class Orders
+    {
+        [Key]
+        public int orderNum { get; set; }
+        public string description { get; set; }
+        public DateTime orderDate { get; set; }
+        public int customerID { get; set; }
+        public virtual customer customer { get; set; }
+
+    }
+}
