54a96d3c54b358311634046fe26c33241740d692
Luce, Thom <luce@ohio.edu>
2020-02-19 10:32:44 -0500

    started user membership

---
+++ Controllers/UserDetailsController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using LuceMIS4200_940.DAL;
+using LuceMIS4200_940.Models;
+using Microsoft.AspNet.Identity;
+
+namespace LuceMIS4200_940.Controllers
+{
+    public class UserDetailsController : Controller
+    {
+        private MIS4200Context db = new MIS4200Context();
+
+        // GET: UserDetails
+        public ActionResult Index()
+        {
+            return View(db.UserDetails.ToList());
+        }
+
+        // GET: UserDetails/Details/5
+        public ActionResult Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserDetails userDetails = db.UserDetails.Find(id);
+            if (userDetails == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userDetails);
+        }
+
+        // GET: UserDetails/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UserDetails/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Email,firstName,lastName,PhoneNumber,Office,Position,hireDate,photo")] UserDetails userDetails)
+        {
+            if (ModelState.IsValid)
+            {
+                Guid memberID;
+                Guid.TryParse(User.Identity.GetUserId(), out memberID);
+                userDetails.ID = memberID;
+                //userDetails.ID = Guid.NewGuid();
+                db.UserDetails.Add(userDetails);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(userDetails);
+        }
+
+        // GET: UserDetails/Edit/5
+        public ActionResult Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserDetails userDetails = db.UserDetails.Find(id);
+            if (userDetails == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userDetails);
+        }
+
+        // POST: UserDetails/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Email,firstName,lastName,PhoneNumber,Office,Position,hireDate,photo")] UserDetails userDetails)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(userDetails).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(userDetails);
+        }
+
+        // GET: UserDetails/Delete/5
+        public ActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserDetails userDetails = db.UserDetails.Find(id);
+            if (userDetails == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userDetails);
+        }
+
+        // POST: UserDetails/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(Guid id)
+        {
+            UserDetails userDetails = db.UserDetails.Find(id);
+            db.UserDetails.Remove(userDetails);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
